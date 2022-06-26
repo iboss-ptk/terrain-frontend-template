1a85e1909fdf94cec17def34a72723b48b7f937f
Michal Turcan <michal.turcan.za@gmail.com>
2022-06-26 13:06:18 +0200

    increasing uluna fee to match luna 2.0 min value

--- src/contract/execute.js
+++ src/contract/execute.js
@@ -1,5 +1,5 @@
-import { LCDClient, MsgExecuteContract, Fee } from "@terra-money/terra.js";
-import { contractAdress } from "./address";
+import { LCDClient, MsgExecuteContract, Fee } from '@terra-money/terra.js';
+import { contractAdress } from './address';
 
 // ==== utils ====
 
@@ -8,7 +8,7 @@ const until = Date.now() + 1000 * 60 * 60;
 const untilInterval = Date.now() + 1000 * 60;
 
 const _exec =
-  (msg, fee = new Fee(200000, { uluna: 10000 })) =>
+  (msg, fee = new Fee(200000, { uluna: 300000 })) =>
   async (wallet) => {
     const lcd = new LCDClient({
       URL: wallet.network.lcd,
